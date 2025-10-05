25189c9530411cd29dfb191b9fc7bc58b2c7d628
saiful10400 <saifulislam10400i5@gmail.com>
2025-10-05 12:14:09 +0600

    content image blur background added.

--- src/components/Component/PostCard/ContentImage.tsx
+++ src/components/Component/PostCard/ContentImage.tsx
@@ -1,16 +1,28 @@
 import Image from 'next/image';
 import React from 'react';
+import "./style.css";
 
-const ContentImage = ({link}:{link:string}) => {
-    return (
-        <Image
-          src={link}
-          alt="Post Image"
-          width={1920}
-          height={1080}
-          className="w-full h-[400px] lg:h-[500px] object-contain rounded-b-xl hover:scale-105 transition-transform duration-500"
-        />
-    );
+const ContentImage = ({ link }: { link: string }) => {
+  return (
+    <div className='w-full h-[400px] lg:h-[500px] relative overflow-hidden'>
+      <Image
+        src={link}
+        alt="Post Image"
+        width={1920}
+        height={1080}
+        className=" w-full h-full object-cover scale-150  transition-transform duration-500"
+      />
+      <div className='postBlurContainer w-full h-full  absolute top-0 right-0'></div>
+      <Image
+        src={link}
+        alt="Post Image"
+        width={1920}
+        height={1080}
+        className="w-full h-full absolute top-0 left-0 lg:h-[500px] object-contain transition-transform duration-500"
+      />
+    </div>
+
+  );
 };
 
 export default ContentImage;
